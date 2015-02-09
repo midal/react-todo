21645268410d8042b1ac0f12f76b55f0392d3192
Mikael Dalkvist <mikael.dalkvist@gmail.com>
2015-02-09 16:46:21 +0100

    Basic API server

--- server/server.js
+++ server/server.js
@@ -2,32 +2,105 @@
 
 'use strict';
 
-var path = require('path');
 var express = require('express');
-// var proxyServer = require('http-route-proxy');
-var server = express();
 
+var app = express();
+var path = require('path');
 var publicFolder = path.join(__dirname, '../');
 
-// console.log(server);
+/* Static */
+app.use(express.static(publicFolder));
+app.disable('x-powered-by');
 
-    server.use(express.static(publicFolder));
-    server.disable('x-powered-by');
-// server.configure(function () {
-//     // Just disable that the "server" is powered by express
-
-//     // Serve static files
-
-//     // // Proxy all requests to /rest to igloo
-//     // server.use(proxyServer.connect({
-//     //     from: 'localhost:8080',
-//     //     to: 'playplus.telia.se:80/rest',
-//     //     https: false,
-//     //     route: ['/rest']
-//     // }));
-// });
-
-server.listen(8080, function () {
+app.listen(8080, function () {
     console.log('Server up and running....');
-    console.log('Serving static files from: ' + publicFolder + ' and forwarding calls to /rest');
+    console.log('Serving static files from: ' + publicFolder);
 });
+
+/* API */
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/react-todo');
+var Todo = require('./todo');
+
+var bodyParser = require('body-parser');
+var port = 8091;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+
+router.route('/todos')
+    // create a todo (accessed at POST http://localhost:8080/api/todos)
+    .post(function(req, res) {
+
+        var todo = new Todo();      // create a new instance of the todo model
+        todo.text = req.body.text;  // set the todos text (comes from the request)
+        todo.done = false;
+
+        // save the todo and check for errors
+        todo.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Todo created!' });
+        });
+
+    })
+    .get(function(req, res) {
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err);
+
+            res.json(todos);
+        });
+    });
+
+router.route('/todos/:todo_id')
+
+    // get the todo with that id (accessed at GET http://localhost:8080/api/todos/:todo_id)
+    .get(function(req, res) {
+        Todo.findById(req.params.todo_id, function(err, todo) {
+            if (err)
+                res.send(err);
+            res.json(todo);
+        });
+    })
+    .put(function(req, res) {
+
+        // use our todo model to find the todo we want
+        Todo.findById(req.params.todo_id, function(err, todo) {
+
+            if (err)
+                res.send(err);
+
+            todo.done = req.body.done;
+
+            // save the todo
+            todo.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Todo updated!' });
+            });
+
+        });
+    });
+
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
