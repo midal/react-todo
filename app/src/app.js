0e2e2eb5b403b1b5f5277b9bf6e2ed9471dc6dee
Mikael Dalkvist <mikael.dalkvist@gmail.com>
2015-03-10 10:35:50 +0100

    Clean up

--- app/src/app.js
+++ app/src/app.js
@@ -3,6 +3,6 @@ var React = require('react');
 var TodoList = require('./components/TodoList.react');
 
 React.render(
-  <TodoList />,
-  document.getElementById('container')
+    <TodoList />,
+    document.getElementById('container')
 );
--- app/src/components/TodoItem.react.js
+++ app/src/components/TodoItem.react.js
@@ -3,46 +3,46 @@ var React = require('react/addons');
 var TodoActions = require('../actions/TodoActions');
 
 var TodoItem = React.createClass({
-  componentDidMount: function() {
-    var that = this;
-    $(this.refs.MyTodoItem.getDOMNode()).swipe( {
-        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-            switch (direction) {
-              case "right":
-                TodoActions.toggleComplete(that.props.data);
-                break;
-              case "left":
-                TodoActions.destroy(that.props.data);
-                break;
+    componentDidMount: function() {
+        var that = this;
+        $(this.refs.MyTodoItem.getDOMNode()).swipe( {
+            swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+                switch (direction) {
+                    case "right":
+                        TodoActions.toggleComplete(that.props.data);
+                        break;
+                    case "left":
+                        TodoActions.destroy(that.props.data);
+                        break;
+                }
+            },
+            swipeStatus: function (event, phase, direction, distance) {
+                if (phase === "move") {
+                    var dir = direction==="right" ? '1' : '-1';
+                    $(this).css({'transform': 'translateX(' + (distance < 200 ? distance : 200)*dir + 'px)'});
+                }
+                else {
+                    $(this).css({'transform': 'translateX(0px)'});
+                }
             }
-        },
-        swipeStatus: function (event, phase, direction, distance) {
-          if (phase === "move") {
-            var dir = direction==="right" ? '1' : '-1';
-            $(this).css({'transform': 'translateX(' + (distance < 200 ? distance : 200)*dir + 'px)'});
-          }
-          else {
-            $(this).css({'transform': 'translateX(0px)'});
-          }
-        }
-    });
-    $(this.refs.MyTodoItem.getDOMNode()).swipe("option", "threshold", 100);
-  },
-  render: function() {
-    var cx = React.addons.classSet;
-    var classes = cx({
-      'todo-item ': true,
-      'done': this.props.data.done
-    });
+        });
+        $(this.refs.MyTodoItem.getDOMNode()).swipe("option", "threshold", 100);
+    },
+    render: function() {
+        var cx = React.addons.classSet;
+        var classes = cx({
+            'todo-item ': true,
+            'done': this.props.data.done
+        });
 
-    return (
-      <li className={classes} ref="MyTodoItem">
-        <div className="pre"></div>
-        <div className="theItem">{this.props.children}</div>
-        <div className="post">Delete</div>
-      </li>
-    );
-  }
+        return (
+            <li className={classes} ref="MyTodoItem">
+                <div className="pre"></div>
+                <div className="theItem">{this.props.children}</div>
+                <div className="post">Delete</div>
+            </li>
+        );
+    }
 });
 
 module.exports = TodoItem;
